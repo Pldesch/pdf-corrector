4a56e3980eabbbc03a0938badb457c1d05558b72
pldesch <pldeschaetzen@gmail.com>
2022-02-03 15:37:43 +0100

    new modif

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       <div className='w-full pt-10 h-full flex flex-col justify-between gap-5 items-center'>
         <input onChange={changeHandler} id='inpFile' type='file' accept='.pdf' className='file:bg-slate-100 file:text-slate-900 file:font-bold file:px-6 file:py-2 file:border-none file:rounded-full bg-slate-600 pr-3 rounded-full' />
         <button type='button' onClick={send} id='btnUpload' className='bg-slate-100 text-slate-900 font-bold px-6 py-2 border-none rounded-full'>Upload</button>
-        <span role='textbox' className='w-full overflow-hidden text-black border-none focus:outline-none shadow-md shadow-slate-600 p-4 block resize bg-white min-h-[100px] rounded-xl' id='resultText' contentEditable>{text}</span>
+        <span  role='textbox' className='w-full overflow-hidden text-white border-slate-400 border-2 focus:outline-none focus:border-slate-100 p-4 block resize bg-slate-600 min-h-[100px] rounded-xl' id='resultText' contentEditable>{text}</span>
       </div>
     </div>
   )
